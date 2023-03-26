752d78cb3f07fb7a598f5e9f54631c225160b743
Rossi21 <rossi21@gmail.com>
2023-03-25 21:04:30 -0700

    First Commit

---
+++ src/main/java/com/rossi21/mvc/services/BookService.java
@@ -0,0 +1,35 @@
+package com.rossi21.mvc.services;
+
+import java.util.Optional;
+
+import com.rossi21.mvc.models.Book;
+import com.rossi21.mvc.repositories.BookRepository;
+
+import antlr.collections.List;
+
+public class BookService {
+	// adding the book repository as a dependency
+    private final BookRepository bookRepository;
+    
+    public BookService(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+    // returns all the books
+    public List<Book> allBooks() {
+        return bookRepository.findAll();
+    }
+    // creates a book
+    public Book createBook(Book b) {
+        return bookRepository.save(b);
+    }
+    // retrieves a book
+    public Book findBook(Long id) {
+        Optional<Book> optionalBook = bookRepository.findById(id);
+        if(optionalBook.isPresent()) {
+            return optionalBook.get();
+        } else {
+            return null;
+        }
+    }
+
+}
